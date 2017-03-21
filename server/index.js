62417dcbdb115ab0d5a503924309a96a934fd9c2
Bart Wijnants <bartwijnants@outlook.com>
2017-03-21 16:56:59 +0100

    listen + endpoints separate

--- server/index.js
+++ server/index.js
@@ -2,35 +2,36 @@ const path = require('path');
 const express = require('express');
 const WebSocket = require('ws');
 
+const PORT = process.env.PORT || 8080
 const app = express();
 
 const sessions = [];
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.listen(3000, () => {
-    console.log('listening on 3000');
-
-    app.post('/api/session', (req, res) => {
-        if (sessions.length === 5) {
-            res.status(500).json({ error: 'Limit of 5 sessions reached.' });
-            return;
-        }
-        const port = 8081 + sessions.length;
-        const users = [];
-        const socket = createSocket(port, users);
-        sessions.push({
-            port: port,
-            users: users,
-            socket: socket
-        });
-        console.log(port);
-        res.status(200).json({ port: port });
+app.post('/api/session', (req, res) => {
+    if (sessions.length === 5) {
+        res.status(500).json({ error: 'Limit of 5 sessions reached.' });
+        return;
+    }
+    const port = 8081 + sessions.length;
+    const users = [];
+    const socket = createSocket(port, users);
+    sessions.push({
+        port: port,
+        users: users,
+        socket: socket
     });
+    console.log(port);
+    res.status(200).json({ port: port });
+});
 
-    app.get('*', (req, res) => {
-        return res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-    });
+app.get('*', (req, res) => {
+    return res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('listening on ' + PORT);
 });
 
 function createSocket(port, users) {
